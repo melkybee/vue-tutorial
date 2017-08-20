80275b800dcc9516e1d8c36938f75961c163e9c5
Melissa Viernes <mbolosan@gmail.com>
2017-08-20 12:46:55 -0700

    Components within components exercise

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-Vue.component('task-list', {
-
-	template: `
-		<div>
-			<task v-for="task in tasks">{{ task.task }}</task>
-		</div>
-	`,
+Vue.component('message', {
+	props: ['title', 'body'],
 
 	data() {
 		return {
-			tasks: [
-				{ task: 'Go to the store', complete: true },
-				{ task: 'Go to the mall', complete: false },
-				{ task: 'Go to the farm', complete: true },
-				{ task: 'Go to work', complete: false }
-			]
+			isVisible: true
+		}
+	},
+
+	template: `
+		<article class="message" v-show="isVisible">
+		  <div class="message-header">
+		    {{ title }}
+		    <button type="button" @click="hideModal">x</button>
+		  </div>
+		  <div class="message-body">
+		    {{ body }}
+		  </div>
+		</article>
+	`,
+
+	methods: {
+		hideModal() {
+			this.isVisible = false;
 		}
 	}
 
